50a9d02123bc34bb0466bea12d70a20cd8f7f4dc
Filip Schenk <schenkofil@gmail.com>
2019-04-25 17:27:21 +0200

    Implementace ošetření inputu

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -28,13 +28,221 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dvojkova = new System.Windows.Forms.CheckBox();
+            this.osmickova = new System.Windows.Forms.CheckBox();
+            this.sestkova = new System.Windows.Forms.CheckBox();
+            this.sestnactkova = new System.Windows.Forms.CheckBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(271, 101);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 57);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Převeď";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(45, 53);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.textBox1.Size = new System.Drawing.Size(296, 26);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(371, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Zadejte nenulové kladné číslo v desítkové soustavě:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(45, 202);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(296, 26);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(45, 279);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(296, 26);
+            this.textBox3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(45, 355);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(296, 26);
+            this.textBox4.TabIndex = 5;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(45, 437);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(296, 26);
+            this.textBox5.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(48, 179);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(256, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Zadané číslo ve dvojkové soustavě:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(48, 256);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(260, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Zadané číslo v osmičkové soustavě:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(48, 332);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(280, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Zadané číslo v šestnáctkové soustavě:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(48, 414);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(249, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Zadané číslo v šestkové soustavě:";
+            // 
+            // dvojkova
+            // 
+            this.dvojkova.AutoSize = true;
+            this.dvojkova.Location = new System.Drawing.Point(16, 101);
+            this.dvojkova.Name = "dvojkova";
+            this.dvojkova.Size = new System.Drawing.Size(96, 24);
+            this.dvojkova.TabIndex = 11;
+            this.dvojkova.Text = "dvojková";
+            this.dvojkova.UseVisualStyleBackColor = true;
+            this.dvojkova.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // osmickova
+            // 
+            this.osmickova.AutoSize = true;
+            this.osmickova.Location = new System.Drawing.Point(16, 143);
+            this.osmickova.Name = "osmickova";
+            this.osmickova.Size = new System.Drawing.Size(109, 24);
+            this.osmickova.TabIndex = 12;
+            this.osmickova.Text = "osmičková";
+            this.osmickova.UseVisualStyleBackColor = true;
+            // 
+            // sestkova
+            // 
+            this.sestkova.AutoSize = true;
+            this.sestkova.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.sestkova.Location = new System.Drawing.Point(128, 101);
+            this.sestkova.Name = "sestkova";
+            this.sestkova.Size = new System.Drawing.Size(98, 24);
+            this.sestkova.TabIndex = 13;
+            this.sestkova.Text = "šestková";
+            this.sestkova.UseVisualStyleBackColor = true;
+            this.sestkova.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // sestnactkova
+            // 
+            this.sestnactkova.AutoSize = true;
+            this.sestnactkova.Location = new System.Drawing.Point(128, 143);
+            this.sestnactkova.Name = "sestnactkova";
+            this.sestnactkova.Size = new System.Drawing.Size(129, 24);
+            this.sestnactkova.TabIndex = 14;
+            this.sestnactkova.Text = "šestnáctková";
+            this.sestnactkova.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(124, 477);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(144, 40);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "              © \r\nSchenaTeam 2019";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(407, 523);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.sestnactkova);
+            this.Controls.Add(this.sestkova);
+            this.Controls.Add(this.osmickova);
+            this.Controls.Add(this.dvojkova);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Přepočet soustav";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox dvojkova;
+        private System.Windows.Forms.CheckBox osmickova;
+        private System.Windows.Forms.CheckBox sestkova;
+        private System.Windows.Forms.CheckBox sestnactkova;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +16,68 @@ namespace WindowsFormsApp1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ulong cislo = 0;
+            if (textBox1.TextLength == 0)
+            {
+                MessageBox.Show("První pole nesmí zůstat nevyplněno!");
+            }
+            if (textBox1.TextLength > 0)
+            {
+                try
+                {
+                    cislo = Convert.ToUInt64(textBox1.Text);
+                }
+                catch (FormatException)
+                {
+                    MessageBox.Show("Musíte zadat pouze čísla");
+                }
+                catch (OverflowException)
+                {
+                    MessageBox.Show("Číslo je příliš velké");
+                }
+            }
+            if (!dvojkova.Checked && !sestkova.Checked && !osmickova.Checked && !sestnactkova.Checked)
+            {
+                MessageBox.Show("Zvolte alespoň jednu soustavu, do které chcete číslo převést!");
+            }
+            
+            if (dvojkova.Checked)
+            {
+                textBox2.Text = cislo.ToString();
+            }
+            if (sestkova.Checked)
+            {
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
